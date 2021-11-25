8971196d8bf8b71b3d4f7ad308605ef810465faf
Smoom <david.tait.96@gmail.com>
2021-11-25 12:37:47 -0700

    converted to jquery elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,62 @@
-var containerEl = document.querySelector(".container");
-containerEl.classList.add("mb-5");
+var containerEl = $(".container");
+
 
 var hoursArr = ["am9", "am10", "am11", "pm12", "pm1", "pm2", "pm3", "pm4", "pm5"];
 var hoursArrReverse = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
-var hoursArmyArry = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+var hoursArmyArr = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 var currentDay = moment();
 var currentHour = moment().hours();
-console.log(currentHour);
 
 var printTable = function() {
   for(var i=0; i < hoursArr.length; i++){
-    var rowEl = document.createElement("div");
-    rowEl.classList.add("row", "mx-auto");
-    rowEl.setAttribute("id", hoursArr[i]);
-    
-    var timeEl = document.createElement("div");
-    timeEl.classList.add("col-1", "border-top", "border-bottom", "border-start");
-    timeEl.textContent = hoursArrReverse[i];
+    //making whole row div
+    var rowEl = $("<div>")
+      .addClass("row mx-auto")
+      .attr("id", hoursArr[i]);
   
-    var taskEl = document.createElement("div");
-    taskEl.classList.add("col-10", "border");
-    if(currentHour > hoursArmyArry[i]){
-      taskEl.classList.add("bg-secondary", "text-white");
-    } else {
-      taskEl.classList.add("bg-success", "text-white");
-    }
-    taskEl.textContent = "Task";
-  
-    var saveEl = document.createElement("div");
-    saveEl.classList.add("col-1", "border-top", "border-bottom", "bg-info");
-    saveEl.textContent = "Save";
-  
-    console.log(saveEl);
+    //making just the time part of the orw
+    var timeEl = $("<div>")
+      .addClass("col-1", "border-top", "border-bottom", "border-start")
+      .text(hoursArrReverse[i]);
 
-    rowEl.appendChild(timeEl);
-    rowEl.appendChild(taskEl);
-    rowEl.appendChild(saveEl);
+    //making just the task area of the row
+    var taskEl = $("<textarea>")
+      .addClass("col-10 border text-center align-middle")
+      .text("Task" + i);
+    if(currentHour > hoursArmyArr[i]){
+      taskEl.addClass("bg-secondary text-white");
+    } else if (currentHour === hoursArmyArr[i]) {
+      taskEl.addClass("bg-warning text-white"); 
+    } else {
+      taskEl.addClass("bg-success text-white");
+    }
+
+    //save button area
+    var saveEl = $("<div>")
+      .addClass("col-1 border-top border-bottom bg-info text-center align-middle");
+    var saveIconEl = $("<i>")
+      .addClass("fas fa-save fa-3x");
+    saveEl.append(saveIconEl);
+
+
+    rowEl.append(timeEl);
+    rowEl.append(taskEl);
+    rowEl.append(saveEl);
   
-  
-    containerEl.appendChild(rowEl);
+    containerEl.append(rowEl);
   };
 };
 
 
 printTable();
+
+$(".row").on("click", ".col-10", function() {
+  var text = $(this).text();
+  console.log(text);
+});
+
+$(".row").on("click", "i", function() {
+  var text = $(".row", ".col-10").text();
+  console.log(text);
+});
